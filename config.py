cfd7bd125a9a59e25b10d2d1d696b9548a45b2b8
gaoguanyu <springaction@foxmail.com>
2014-11-22 16:46:29 +0800

    dd

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 #the configuration of the master node
 master_ip   = "155.69.149.156"
-master_port = "8090"
+master_rpc_port = "8090"
+master_rev_port = "9000"
+master_snd_port = "9010"
 master_path = "/tmp/master/"
 
 #the configuration of the worker node
