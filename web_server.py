112973d58968ce44d84c9f6aeb282f20b50e3d25
springoverlake <springoverlake@gmail.com>
2015-04-12 12:16:27 +0000

    home page

--- web_server.py
+++ web_server.py
@@ -14,24 +14,9 @@ urls = (
 
 class home_page:
     def GET(self):
-	msg = '<!DOCTYPE html> \n' + \
-		'<html> \n' + \
-		'<head> \n' + \
-		'<style> \n' + \
-		'table, th, td { \n' + \
-		'border: 1px solid black; \n' + \
-		'border-collapse: collapse; \n' + \
-		'}\n' + \
-		'th, td { \n' + \
-		'padding: 5px; \n' + \
-		'} \n' + \
-		'</style> \n' + \
-		'</head> \n' + \
-		'<body> \n'
-
-	msg += '<table style=\"width:10%\"> \n'
-	msg += '<tr> \n <th>Task ID</th> \n <th>Progress</th> \n </tr>'
-        for fname in os.listdir(basepath):
+	task_list = []
+	render = web.template.render('/var/www/webpy-app/templates/')
+	for fname in os.listdir(basepath):
             path = os.path.join(basepath, fname)
             if os.path.isdir(path):
                 continue
@@ -41,18 +26,8 @@ class home_page:
                 pkl_file = open(path, 'rb')
                 task = dill.load(pkl_file)
                 pkl_file.close()
-		msg += '<tr>\n'
-		msg += '<td>\n'
-		msg += task.task_id + '\n'
-		msg += '</td>\n'
-		msg += '<td>\n'
-		msg += str(task.progress) + '\n'
-		msg += '</td>\n'
-		msg += '</tr>'
-	msg += '</table> \n'
-	msg += '</body> \n'
-	msg += '</html> \n'
-	return msg
+		task_list.append(task)
+	return render.home(task_list)
 
 
 application = web.application(urls, globals()).wsgifunc()
