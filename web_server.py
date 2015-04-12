474fcd91f2556416a9d27e036adfee73d95d5e8e
gaoguanyu <springaction@foxmail.com>
2015-04-12 13:21:12 +0800

    revise the web app

--- web_server.py
+++ web_server.py
@@ -1,11 +1,20 @@
 import web
 
-urls = (
-    '/.*', 'hello',)
+basepath = '/data/master/'
 
-class hello:
+urls = (
+    '/.*', 'home_page',)
+
+class home_page:
     def GET(self):
-        return "Hello, world."
+        for fname in os.listdir(basepath):
+            path = os.path.join(basepath, fname)
+            if os.path.isdir(path):
+                continue
+
+            _, ext = os.path.splitext(path)
+            if ext == '.pkl':
+                info = c.probe(path)
 
 
 application = web.application(urls, globals()).wsgifunc()
