bceacbb11b7cddcffe1feb520a106d4da90ae817
gaoguanyu <springaction@foxmail.com>
2015-06-29 18:38:13 +0800

    the estimator can work now

--- algorithms/estimator.py
+++ algorithms/estimator.py
@@ -4,23 +4,29 @@ import scipy.io
 import numpy as np
 import neurolab as nl
 
-input   = scipy.io.loadmat('../data/input.mat')
-target  = scipy.io.loadmat('../data/target.mat')
-input   = input['input'].transpose()
-target  = target['t']
+input   = scipy.io.loadmat('input.mat')
+target  = scipy.io.loadmat('target.mat')
+i = input['input'].transpose()
+t = target['t']
 
+'''
+print target
 print input.shape
 print target.shape
 sys.exit()
+'''
 
-net = nl.net.newff([[400, 3500], [9, 13000]], [30, 1])
-err = net.train(input, target, epochs=500, show=1)
+norm_t = nl.tool.Norm(t)
+t = norm_t(t)
 
-out = net.sim(input)
-print out
+net = nl.net.newff(nl.tool.minmax(i), [10, 1])
+err = net.train(i, t, epochs=500, show=1)
+
+out = net.sim(i)
+out = norm_t.renorm(out)
+
+print target['t'] - out
 sys.exit()
 
-for i in range(490):
-    print input[i], target[i], '---', out[i]
 
 
