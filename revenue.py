884728bb42931aa2ea52b030f6cd778ed3b5a06f
gaoguanyu <springaction@foxmail.com>
2015-07-01 15:08:28 +0800

    function for calculating revenue

--- revenue.py
+++ revenue.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sqlite3 as lite
 import sys
-
+import config
+import sqlite3 as lite
 
 def period_revenue(start_time, end_time):
     con = None
     try:
-        con = lite.connect('test.db')
+        con = lite.connect(config.db_name)
         cur = con.cursor()
-        sql_cmd = 'SELECT * FROM Cars WHERE Cost BETWEEN 20000 AND 55000'
+        sql_cmd = 'SELECT * FROM task_info WHERE finish_time BETWEEN %f AND %f' % \
+                (start_time, end_time)
         cur.execute(sql_cmd)
         rows = cur.fetchall()
         for row in rows:
             print "%s %s %s" % (row[0], row[1], row[2])
-            print row
 
     except lite.Error, e:
         print "Error %s:" % e.args[0]
         sys.exit(1)
-
     finally:
         if con:
             con.close()
 
-period_revenue(0,1)
+period_revenue(1435731814.49739, 1435732481.50626)
 
