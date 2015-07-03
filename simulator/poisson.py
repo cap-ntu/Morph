0aabe3ee13614ae1efe0a9da5afc190eb3c97483
gaoguanyu <springaction@foxmail.com>
2015-07-03 21:56:48 +0800

    add

--- simulator/poisson.py
+++ simulator/poisson.py
@@ -12,7 +12,7 @@ pending_task = []
 finished_tasks = []
 machine_num = 10
 duration = 60 * 100
-arrive_rate = 1.0 / (60.0 * 3.0)
+arrive_rate = 1.2 / (75.0)
 seg_trans_time = 60.0
 decay_factor = 0.99
 
@@ -32,11 +32,11 @@ while t < duration:
     x = task()
     x.priority = random.randint(1, 3)
     x.start_time = int(t)
-    x.block_num = random.randint(5, 20)
+    x.block_num = random.randint(4, 20)
     all_task.append(x)
 
+
 print len(all_task)
-sys.exit()
 
 t = 0
 a_t = 0
@@ -51,20 +51,22 @@ while t < duration:
 
     scheduling.fifo(pending_task)
 
+    '''
     print 'current time:', t, ':',
     for x in pending_task:
         print x.start_time, ' ',
     print ' '
+    '''
 
     if a_t <= t:
         if len(pending_task) == 0:
             a_t = t
-            break
         else:
             task = pending_task.pop(0)
-            a_t = task.block_num * seg_trans_time / machine_num + t
+            #print 'execute a task:', t
+            print len(pending_task)
+            a_t = task.block_num * seg_trans_time * 1.0 / machine_num + t
+            #print 'estimated time:', a_t
 
     t = t + 1
-    #time.sleep(1)
-
 
