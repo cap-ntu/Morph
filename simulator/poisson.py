19b8216f4ec6fe0958254e2ad67d03e99212d1dc
gaoguanyu <springaction@foxmail.com>
2015-07-03 19:39:31 +0800

    update

--- simulator/poisson.py
+++ simulator/poisson.py
@@ -7,6 +7,10 @@ all_task = []
 pending_taks = []
 finished_tasks = []
 machine_num = 10
+duration = 100
+arrive_rate = 1
+
+decay_factor = 0.99
 
 class task:
     def __init__(self):
@@ -17,21 +21,31 @@ class task:
         self.deadline   = 0     #the deadline for this task
         self.value      = 0     #the estimated value for scheduling
 
-while t < 60:
-    t = t + random.expovariate(1.0)
+def sched_algo(pending_task):
+
+
+#generate the tasks
+while t < duration:
+    t = t + random.expovariate(arrive_rate)
     x = task()
     x.priority = random.randint(1, 3)
     x.start_time = t
     x.block_num = random.randint(5, 20)
     all_task.append(x)
 
+
 t = 0
-while t < 60:
-    pending_task = all_task
-    pending_task = [x for x in pending_task if x.start_time <= t]
+next_a_t = 0
 
+while t < duration:
+    pending_task = [x for x in all_task if x.start_time <= t]
 
+    print 'current time:', t, ' ',
+    for x in pending_task:
+        print x.start_time, ' ',
+    print ' '
 
-    print len(pending_task)
-    time.sleep(1)
     t = t + 1
+    time.sleep(2)
+
+
