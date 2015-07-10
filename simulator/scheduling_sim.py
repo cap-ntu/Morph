b4966e0f7e936512358eb572fd4cfef0fa7e4011
gaoguanyu <springaction@foxmail.com>
2015-07-10 22:13:22 +0800

    new

--- simulator/scheduling_sim.py
+++ simulator/scheduling_sim.py
@@ -24,15 +24,14 @@ t = 0
 all_task = []
 pending_task = []
 
-machine_num = 10
+machine_num = 15
 sim_dur = 60 * 60
 
 
 price_per_type = config.price_per_type
 
-arrive_rate = 5 / (60.0*3.0)
-decay_factor = config.price_decaying
-equal_trans_dur  = config.equal_trans_dur
+arrive_rate      = 2 / 60.0
+decay_factor     = config.price_decaying
 vm_cost_per_hour = config.vm_cost_per_hour
 
 class task:
@@ -40,7 +39,7 @@ class task:
         self.task_id    = 0     #task id
         self.priority   = 0     #task priority
         self.start_time = 0     #the time of adding the transcoding task
-        self.block_num  = -1    #the total number of video blocks
+        self.est_time   = -1    #the total number of video blocks
         self.end_time   = 0     #the finish time of the task
         self.deadline   = 0     #the deadline for this task
         self.value      = 0     #the estimated value for scheduling
@@ -75,20 +74,20 @@ if flag == True:
     while t < sim_dur:
         t = t + random.expovariate(arrive_rate)
         x = task()
-        x.task_id  = task_id
-        x.priority = gen_p()
+        x.task_id    = task_id
+        x.priority   = gen_p()
         x.start_time = int(t)
-        x.block_num  = random.randint(1, 10)
-        x.deadline   = int(x.start_time + x.block_num * equal_trans_dur * 3.0)
+        x.est_time   = random.randint(180, 1800) * 1.0
+        x.deadline   = int(x.start_time + x.est_time * 3.0)
         all_task.append(x)
         task_id = task_id + 1
 
-    f = open('all_task.pkl','wb')
+    f = open('all_task_sim_sch.pkl','wb')
     pickle.dump(all_task, f)
     f.close()
 
 
-f = open('all_task.pkl', 'r')
+f = open('all_task_sim_sch.pkl', 'r')
 all_task = pickle.load(f)
 sum_revenue = 0
 
@@ -110,7 +109,7 @@ while t < sim_dur + 60*60*2:
     if algo == 'vbs':
         scheduling.schedule_task[algo](pending_task, t, machine_num)
     elif algo == 'hvs':
-        scheduling.schedule_task[algo](pending_task, t)
+        scheduling.schedule_task[algo](pending_task, t, machine_num)
     else:
         scheduling.schedule_task[algo](pending_task)
 
@@ -121,14 +120,17 @@ while t < sim_dur + 60*60*2:
             task = pending_task.pop(0)
             #print 'execute a task:', t
             #print len(pending_task)
-            a_t = task.block_num * equal_trans_dur * 1.0 / machine_num + t
-            sum_revenue = sum_revenue + price_per_type[task.priority] * task.block_num * (equal_trans_dur / 60.0) \
+            a_t = task.est_time * 1.0 / machine_num + t
+            sum_revenue = sum_revenue + price_per_type[task.priority] * (task.est_time / 60.0) \
                                 * math.pow(decay_factor, (a_t - task.start_time))
             #print 'estimated time:', a_t
             latency[task.priority] += (t - task.start_time)
             req_num[task.priority] += 1
 
     t = t + 1
+
+print '-----------'
+print algo
 
 print 'pending task number', len(pending_task)
 print 'total revenue:', sum_revenue
@@ -137,4 +139,6 @@ print 'total revenue:', sum_revenue
 
 for key in latency.keys():
     print key, '  ' ,latency[key] * 1.0 / req_num[key]
+
+
 
