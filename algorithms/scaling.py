a57308a90e02d6643fa5b7bea95e4872367b5fa7
gaoguanyu <springaction@foxmail.com>
2015-07-17 23:43:37 +0800

    for resource management

--- algorithms/scaling.py
+++ algorithms/scaling.py
@@ -1,2 +1,25 @@
+import sys
+import redis
+sys.path.append("../.")
+import config
+
+list_name = 'vm_list'
+
+redis_ip = config.master_ip
+redis_ip = 'localhost'
+r = redis.StrictRedis(host=redis_ip, port=6379, db=0)
+r.delete(list_name)
+
+
+f = open('vm.list', 'r')
+lines = f.readlines()
+for line in lines:
+    line = line.strip('\n')
+    r.rpush(list_name, line)
+
+
+
+print r.lrange(list_name, 0, -1)
+
 
 
