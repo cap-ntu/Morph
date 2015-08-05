e2687f729ed998f3533c524b71cc9ad2a5451fde
springoverlake <springoverlake@gmail.com>
2015-08-05 07:45:58 +0000

    new information

--- statis-2.py
+++ statis-2.py
@@ -15,6 +15,18 @@ vm_cost = config.vm_cost_per_hour
 db_name = sys.argv[1]
 m_num = [11, 6, 13, 8, 5, 6, 10, 17, 11, 13, 8, 16, 20, 15, 13, 3, 16, 9, 20, 20, 8, 17, 8, 9];
 
+#m_num = [11, 6, 13, 7, 5, 6, 10, 17, 13, 14, 8, 14, 21, 15, 10, 4, 14, 11, 23, 19, 8, 18, 8, 10];
+
+#for hvs
+m_num = [11, 6, 13, 7, 7, 6, 13, 17, 11, 14, 8, 16, 20, 14, 10, 3, 16, 9, 21, 21, 8, 18, 8, 11];
+
+#rate_set = [4, 2, 4, 2, 1, 2, 3, 5, 3, 4, 2, 5, 6, 5, 3, 1, 5, 3, 7, 6, 2, 6, 2, 3]
+#m_num = [x*3 for x in rate_set]
+
+#for vbs_new_policy
+#m_num = [10, 5, 12, 6, 5, 5, 11, 15, 9, 14, 7, 14, 19, 14, 10, 4, 14, 9, 19, 19, 8, 17, 9,8];
+
+
 def period_revenue(start_time, end_time):
     con = None
     cost = 0
@@ -32,8 +44,10 @@ def period_revenue(start_time, end_time):
 
         for k in range(0, 24):
             #cost = 15 * (k+1) * vm_cost / 2.0
-            cost = 10 * (k+1) * vm_cost / 2.0
-            #cost = sum(m_num[0:k+1]) * vm_cost / 2.0
+            #cost = 10 * (k+1) * vm_cost / 2.0
+            #cost = 5 * (k+1) * vm_cost / 2.0
+            cost = sum(m_num[0:k+1]) * vm_cost / 2.0
+
             revenue = 0
             epch_start = start_time
             epch_end   = start_time + (k + 1) * 1800
@@ -53,7 +67,7 @@ def period_revenue(start_time, end_time):
                     revenue += task_revenue
                     task_num += 1
                     #print row[4]
-            print (revenue - cost)
+            print (revenue - cost) * 0.95
 
 
 
