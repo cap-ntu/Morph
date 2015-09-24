c8cefd562ccd4eb29d86979c0687cdeabf2f88a5
springoverlake <springoverlake@gmail.com>
2015-09-24 08:57:59 +0000

    Tran the neural network and evaluate the performance of the transcoding time estimation

--- algorithms/estimating.py
+++ algorithms/estimating.py
@@ -1,12 +1,18 @@
+'''
+Tran the neural network and evaluate the performance of the transcoding time estimation
+'''
+
 #!/usr/bin/python
 import sys
 import scipy.io
 import numpy as np
 import neurolab as nl
 
-
+#the input feature vectors of each training instance
 input   = scipy.io.loadmat('t3_input.mat')
+#the real transcoding time of each training instance
 target  = scipy.io.loadmat('t3_output.mat')
+
 #i = input['transcoding1to5'].transpose()
 i = input['t3_input']
 t = target['t3_output']
