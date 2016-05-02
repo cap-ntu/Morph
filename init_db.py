867acd5f8a2faab00fd73c79ce0509bb7c5869f2
springlake <544282962@qq.com>
2016-05-02 12:53:27 -0700

    initialize the tables in the database

--- init_db.py
+++ init_db.py
@@ -24,17 +24,25 @@ print 'database name:' + db_name
 if __name__ == '__main__':
 
     db = MySQLdb.connect(ip, user_name, passwd)
-    cursor = db.cursor()
+    with db:
 
-    print 'create the database in Mysql:' + db_name
-    sql = 'create database if not exists ' + db_name
-    cursor.execute(sql)
+        cursor = db.cursor()
 
-    cur.execute("create table if not exists task_info(id TEXT, submit_time REAL, start_time REAL, \
-                finish_time REAL, service_type INTEGER, trans_time REAL, task_ongoing INTEGER)")
-    con.commit()
+        print 'create the database in Mysql:' + db_name
+        sql = 'create database if not exists ' + db_name
+        cursor.execute(sql)
 
+        print 'use the database:' + db_name
+        sql = 'use ' + db_name
+        cursor.execute(sql)
 
+        print 'create table:' + 'task_info'
+        sql = "create table if not exists task_info(id TEXT, submit_time REAL, start_time REAL, \
+                finish_time REAL, service_type INTEGER, trans_time REAL, task_ongoing INTEGER)"
+        cursor.execute(sql)
+
+        sql = "create table if not exists server_info(server_num INTEGER, server_list TEXT, id TEXT)"
+        cursor.execute(sql)
 
 
 
