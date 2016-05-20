471cfeced82d10d50ea96281250ca5f6781c02bf
springlake <544282962@qq.com>
2016-05-20 00:55:03 -0700

    create work path

--- config.py
+++ config.py
@@ -8,15 +8,15 @@ master_ip       = "127.0.0.1"
 master_rpc_port = "8091"
 master_rev_port = "9001"
 master_snd_port = "9011"
-master_path = "./master/"
+master_path = "./master_data/"
 
 #the configuration of the worker node
-worker_path = "./worker/"
+worker_path = "./worker_data/"
 
 #the configuration of Mysql
 mysql_ip        = "127.0.0.1"
 mysql_user_name = "root"
-mysql_password  = ""
+mysql_password  = "123"
 mysql_db_name   = "morph"
 
 #the duration for each of the video block
@@ -31,4 +31,11 @@ sch_alg = 'fifo'
 
 master_path = os.path.abspath(master_path)
 worker_path = os.path.abspath(worker_path)
+
+if not os.path.exists(master_path):
+    os.makedirs(master_path)
+
+if not os.path.exists(worker_path):
+    os.makedirs(worker_path)
+
 
