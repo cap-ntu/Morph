d2b049ba6d7378e0957493d1a568a13e740f798d
springlake <544282962@qq.com>
2016-05-27 02:48:30 -0700

    testing

---
+++ test.py
@@ -0,0 +1,40 @@
+'''
+Author: Guanyu Gao
+Email:  guanyugao@gmail.com
+Description:
+Resource management for controlling the transcoding workers
+'''
+
+import sys
+import time
+import config
+import MySQLdb
+
+ip        = config.mysql_ip
+passwd    = config.mysql_password
+user_name = config.mysql_user_name
+db_name   = config.mysql_db_name
+
+'''
+get the current number of pending transcoding tasks
+'''
+def get_pending_task():
+    try:
+        con = MySQLdb.connect(ip, user_name, passwd, db_name)
+        cur = con.cursor()
+        sql_cmd = 'SELECT * FROM task_info WHERE task_ongoing = 1'
+        cur.execute(sql_cmd)
+        rows = cur.fetchall()
+        print rows
+        con.close()
+        return ret
+    except Exception, e:
+        print str(e)
+        return -1
+
+
+if __name__ == '__main__':
+    get_pending_task()
+
+
+
